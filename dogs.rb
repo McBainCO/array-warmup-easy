61b42ba43e3048abf2862bc842deeb31b5482683
Graham McBaingrahammcbain@gmail.comgrahammcbain@gmail.comgit config --global user.email your_email@example.comgit config --global user.name Graham <grahammcbain@gmail.comgit config --global user.emailgrahammcbain@gmail.comgit config --global user.email grahammcbain@gmail.com>
2014-06-18 11:49:06 -0600

    dog array excercise

--- dogs.rb
+++ dogs.rb
@@ -1,37 +1,50 @@
 require_relative "testing_library"
 
 dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
+my_dog = ["Reggie"]
 
 def how_many_dogs(dogs)
-
+  dogs.length
 end
 
-def name_lengths(dogs)
 
+def name_lengths(dogs)
+  dog_l = dogs.dup
+  dog_l.map do |d|
+    d.length
+  end
 end
 
 def reverse_dog_names(dogs)
-
+  dogs.map {|r| r.reverse}
 end
 
 def first_three_dogs_with_each(dogs)
-
+  dog_t = dogs.dup
+  dog_t.map {|t| t[0..2]}
 end
 
 def first_three_dogs_without_each(dogs)
-
+  #dogs_ft = dogs
+  #dogs_ft[0..2]
+  dogs[0..2]
 end
 
 def reverse_case_dog_names(dogs)
-
+  #dog_c = dogs
+  dogs.map {|c| c.swapcase}
 end
 
 def sum_of_all_dog_name_lengths(dogs)
-
+  #this is called reduce
+  sum = 0
+  dogs.each do |dog_name|
+    sum = sum + dog_name.length
+  end
 end
 
 def dogs_with_long_names(dogs)
-
+  dogs.map {|dog_name| dog_name.length > 4}
 end
 
 puts "*"*80
@@ -40,6 +53,8 @@ puts "The check method will run and tell you if the answer is correct"
 puts "*"*80
 
 check("how_many_dogs", how_many_dogs(dogs) == 6)
+check("how_many_dogs", how_many_dogs(my_dog) == 1)
+
 check("name_lengths", name_lengths(dogs) == [4, 8, 4, 6, 4, 7])
 check("reverse_dog_names", reverse_dog_names(dogs) == ["odiF", "hgielraH", "ilaM", "eixirT", "wonS", "yrotciV"])
 check("first_three_dogs_with_each", first_three_dogs_with_each(dogs) == ["Fido", "Harleigh", "Mali"])
